b527aca3b255c207674cc9149e7bb06c2d0f1d71
JR Priestman <jpriest8487@gmail.com>
2019-03-04 15:14:10 -0500

    collection

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import collectionReducer from './collectionReducer';
 
 
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    coll: collectionReducer
 })
 
 
