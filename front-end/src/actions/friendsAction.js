a38c3d6d503ce423232d9707e9fa6f5572e01cbe
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-06 10:50:39 -0500

    Created action and reducer for Friends | Figuring out how to pass friend info

--- front-end/src/actions/friendsAction.js
+++ front-end/src/actions/friendsAction.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export default ()=>{
     const axiosPromise = axios({
-        url: `${window.apiHost}/users/collection`,
+        url: `${window.apiHost}/users/friends`,
         method: 'GET',
     })
     return{
-        type: "COLLECTION_ACTION",
+        type: "GET_FRIENDS_ACTION",
         payload: axiosPromise
     }
 }
